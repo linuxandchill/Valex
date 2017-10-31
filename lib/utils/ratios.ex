51de871d3078e9caa3fd9e9502e28e5f8ec87520
linuxandchill <rivercodi@gmail.com>
2017-10-31 07:51:53 -0700

    eps

--- lib/utils/ratios.ex
+++ lib/utils/ratios.ex
@@ -4,27 +4,31 @@ defmodule Ratios do
 
   #net income / shares outstanding
   def eps() do 
-    %HTTPoison.Response{body: net_income} = 
-      HTTPoison.get! "https://api.intrinio.com/" <> "data_point?identifier=AAPL&item=netincome", @headers
-    net_income = net_income |> Poison.decode! |> Map.get("value") |> round
+     ticker = IO.gets "Enter symbol: "
+     ticker = ticker
+     |> String.upcase
+     |> String.trim
+     %HTTPoison.Response{body: net_income} = 
+       HTTPoison.get! "https://api.intrinio.com/" <> "data_point?identifier=#{ticker}&item=netincome", @headers
+       net_income = net_income |> Poison.decode! |> Map.get("value") |> round
 
-    %HTTPoison.Response{body: shares_os} = 
-      HTTPoison.get! "https://api.intrinio.com/" <> "data_point?identifier=AAPL&item=weightedavedilutedsharesos", @headers
-    shares_os = shares_os |> Poison.decode! |> Map.get("value") |> round
-    
-    net_income / shares_os |> Float.round(2)
+      %HTTPoison.Response{body: shares_os} = 
+        HTTPoison.get! "https://api.intrinio.com/" <> "data_point?identifier=#{ticker}&item=weightedavedilutedsharesos", @headers
+      shares_os = shares_os |> Poison.decode! |> Map.get("value") |> round
+      
+      net_income / shares_os |> Float.round(2)
   end
 
   #price / eps
-  def pe do 
-    %HTTPoison.Response{body: price} = 
-      HTTPoison.get! "https://api.intrinio.com/" <> "data_point?identifier=AAPL&item=last_price", @headers
-    price = price |> Poison.decode! |> Map.get("value")  
-
-    price / eps() |> Float.round(2)
-    
-  end
-
+  #  def pe do 
+  #    %HTTPoison.Response{body: price} = 
+  #      HTTPoison.get! "https://api.intrinio.com/" <> "data_point?identifier=AAPL&item=last_price", @headers
+  #    price = price |> Poison.decode! |> Map.get("value")  
+  #
+  #    price / eps() |> Float.round(2)
+  #    
+  #  end
+  #
   def current_ratio do 
     #curr_ass / curr_liabs
   end
